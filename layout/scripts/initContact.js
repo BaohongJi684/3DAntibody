34284b4a0d2585d4059d598b22b2e8d2e0e916f6
BaohongJi684 <jibaohong@hotmail.com>
2026-03-14 17:34:31 -0400

    formbutton

--- layout/scripts/initContact.js
+++ layout/scripts/initContact.js
@@ -8,6 +8,25 @@
     } else {
       document.getElementById("request-type").value = "Sequencing";
     }
+      const form = document.getElementById('messageForm');
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          //alert("Thanks for your submission!");
+          document.getElementById('emailConfirm').style.display = "block";
+          form.reset();
+        }
+      });
+    }
+    form.addEventListener('submit', handleSubmit);
   };
   function initMainFunction() {
     requestTypeSequencing = true;
